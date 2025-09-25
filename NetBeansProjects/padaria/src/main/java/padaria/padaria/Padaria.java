63ac47f7d78be67f972e5b89b6ec3701c00880bb
Miguel <miguelmassane05@gmail.com>
2025-09-24 21:51:58 -0300

    Aula 24/09/2025

---
+++ NetBeansProjects/padaria/src/main/java/padaria/padaria/Padaria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package padaria.padaria;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Java
+ */
+
+
---
+++ NetBeansProjects/padaria/src/main/java/padaria/padaria/Produto.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package padaria.padaria;
+
+/**
+ *
+ * @author Java
+ */
+
+public class Produto {
+
+    private int codigo;
+    private int preco;
+    private String descricao;
+
+    public Produto(int codigo, int preco, String descricao) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+    
+    public int getcodigo() {
+        return codigo;
+    }
+
+
+    public void setcodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
